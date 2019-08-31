84a8aed1259874f0bb6f1dfb7c360a7bd468eef5
linvain <linvain@protonmail.com>
2019-08-31 21:21:53 +0300

    refactor

---
+++ src/TOKEN_TYPES.js
@@ -0,0 +1,8 @@
+module.exports.TOKEN_TYPES = {
+  SEMICOLON: ';',
+  IDENT: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  COMMENT: '#',
+  STRING_CONTENT: '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&()*+,-./:;<=>?@[]^_`{|}~ ',
+  STRING_DELIMITER: '"',
+  INTEGER: '0123456789'
+}
---
+++ src/inputStream.js
@@ -0,0 +1,32 @@
+module.exports.inputStream = input => {
+  let pos = 0;
+  let line = 1;
+  let col = 0;
+
+  const next = () => {
+    const char = input.charAt(pos);
+    pos += 1;
+    if (char === '\n') {
+      line += 1;
+      col = 0;
+    } else {
+      col += 1;
+    }
+    return char
+  }
+
+  const peek = () => input.charAt(pos);
+
+  const eof = () => peek() === "";
+
+  const croak = msg => {
+    throw new Error(msg + " (" + line + ":" + col + ")");
+  }
+
+  return {
+    next,
+    peek,
+    eof,
+    croak,
+  }
+}
