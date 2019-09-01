ba31e55dda4540a40d0a354cfb11aa777c3eb403
linvain <linvain@protonmail.com>
2019-09-01 11:32:56 +0300

    test dist

--- src/parse.test.js
+++ src/parse.test.js
@@ -1,5 +1,12 @@
 import { parse } from './parse';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(parse("kek = 1")).toBe([]);
+test('parses "one = 1;"', () => {
+  const expected = [
+    { type: 'IDENT', value: 'one' },
+    { type: 'OPERATOR', value: '=' },
+    { type: 'INTEGER', value: 1 },
+    { type: 'PUNCTUATION', value: ';' }
+  ];
+
+  expect(parse("one = 1;")).toStrictEqual(expected);
 });
